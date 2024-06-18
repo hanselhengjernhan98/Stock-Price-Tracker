87b644e682123e300fc5fca217d4427dbe2c5b21
hanselhengjernhan98 <70045432+hanselhengjernhan98@users.noreply.github.com>
2024-06-18 13:49:09 +0800

    Add files via upload

---
+++ Instructions.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Instructions = () => {
+  return <div>Select xxxx</div>;
+};
+
+export default Instructions;
---
+++ Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>"Welcome! Please select what you'll want to do"</div>;
+};
+
+export default Login;
